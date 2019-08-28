279a986f135914a1bbe1a55dfb4b76527c356b51
lama <lamasonmez@gmail.com>
2019-08-28 23:12:57 +0300

    add validation to articles_form

--- src/components/articles/_partials/article_form.vue
+++ src/components/articles/_partials/article_form.vue
@@ -1,24 +1,24 @@
 <template>
-  <form id="articleForm" novalidate  name="form_article" @submit.prevent="storeArticle(article)">
+  <form id="articleForm" novalidate  name="form_article" @submit.prevent="validateForm()">
     <div class="row">
       <div class="form-group col-12">
         <label>Title : </label>
-        <input type="text" class="form-control" v-model="article.title" required>
+        <input type="text" class="form-control" v-model="article.title" :class="{'has-error':isEmptyTitle==true}" required>
       </div>
 
       <div class="form-group col-12">
         <label>Date : </label>
-        <input type="date" class="form-control" v-model="article.date" required>
+        <input type="date" class="form-control" v-model="article.date" :class="{'has-error':isEmptyDate==true}" required>
       </div>
       <div class="form-group col-12">
         <label>Content : </label>
-        <editor api-key="17a7w22618yydnhvp25lt3ebumk6ayc26srktyfnje2pwmdd" required :init="{plugins: 'wordcount'}"
+        <editor api-key="17a7w22618yydnhvp25lt3ebumk6ayc26srktyfnje2pwmdd" :class="{'has-error':isEmptyContent==true}" required :init="{plugins: 'wordcount'}"
           v-model="article.content"></editor>
 
       </div>
       <div class="form-group col-8">
         <label class="form-label">Choose Image : </label>
-        <input type="file" id="image" class="form-control" @change="uploadImage(this)" required>
+        <input type="file" id="image" class="form-control" :class="{'has-error':isBigSizeImage==true || isEmptyImage}" @change="uploadImage(this)" required>
       </div>
       <div class="col-4 img-preview">
         <div class="image-container">
@@ -31,7 +31,7 @@
         <p v-if="error">{{error}}</p>
       </div>
       <div class="form-group col-12">
-        <button type="submit" class="btn btn-primary">
+        <button type="submit" class="btn btn-primary" :disabled="isBigSizeImage">
           <span>Save</span>
           <span class="fas fa-save icon"></span>
         </button>
@@ -69,6 +69,15 @@
       return {
         isloading: false,
         error:null,
+        attemptSubmit: false,
+        isBigSizeImage:false,
+        isEmptyTitle:false,
+        isEmptyContent:false,
+        isEmptyDate:false,
+        isEmptyImage:false
+
+
+
       }
     },
     props: {
@@ -89,12 +98,13 @@
         var file = choosenImageElement.files[0];
         if(file.size>4096){
             this.error = "The Image Size Must Be Less Than 4M Please choose another file";
-            
+            this.isBigSizeImage= true
         }
         else{
             this.error=null;
-        var reader = new FileReader();
-        var picFile;
+            this.isBigSizeImage= false;
+          var reader = new FileReader();
+          var picFile;
         reader.addEventListener("load", (event) => {
           picFile = event.target;
           this.article.image = picFile.result;
@@ -104,9 +114,18 @@
         }
        
       },
-      ...mapActions({
-        storeArticle: 'storeArticle'
-      })
+      validateForm:function(event){
+        this.attemptSubmit = true;
+         if(this.article.title==null){this.isEmptyTitle =true;} else this.isEmptyTitle = false;
+         if(this.article.content==null){this.isEmptyContent =true;} else this.isEmptyContent = false;
+         if(this.article.date==null){this.isEmptyDate =true;} else this.isEmptyDate = false;
+         if(this.article.image==null){this.isEmptyImage =true;} else this.isEmptyImage = false;
+        
+        if (this.isBigSizeImage || this.isEmptyTitle || this.isEmptyContent || this.isEmptyDate || this.isEmptyImage) event.preventDefault();
+        else{
+          this.$store.dispatch('storeArticle',this.article);
+        }
+      },
     },
     computed: mapState({
       inProcess: state => state.article.inProcess,
@@ -137,6 +156,10 @@
         border: 1px solid #ced4da;
         border-radius: 0.25rem;
         height: 40px;
+        &.has-error{
+          border-color: #a94442;
+          @include prefixer(box-shadow,inset 0 1px 1px rgba(0,0,0,.075),($prefix-list));
+        }
       }
 
       textarea {

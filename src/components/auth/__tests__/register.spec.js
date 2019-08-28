69265493d9d239e16e36de2ad1e6df117aa5ba1a
lama <lamasonmez@gmail.com>
2019-08-28 20:40:41 +0300

    add register form testing

---
+++ src/components/auth/__tests__/register.spec.js
@@ -0,0 +1,36 @@
+import Vuex from 'vuex'
+import { createLocalVue, shallowMount } from '@vue/test-utils'
+import Register from '@/components/auth/register'
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+
+describe('Register Form', () => {
+    let actions
+    let store
+    beforeEach(() => {
+        actions = {
+                register: jest.fn(() => Promise.resolve())
+            },
+            store = new Vuex.Store({
+                actions
+            })
+    })
+
+    it('calls the register action when the form is submited', () => {
+        const wrapper = shallowMount(Register, {
+            store,
+            localVue,
+            computed: {
+                inProcess: () => false,
+            }
+
+        })
+        wrapper.find('form').trigger('submit')
+        expect(actions.register).toHaveBeenCalled()
+    })
+
+
+
+})
